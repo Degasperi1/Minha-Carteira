e93b084de8cf579afa193693d14358c937b5fe16
Degasperi <celso.gasperi@universo.univates.br>
2021-03-24 21:45:25 -0300

    FEATURE: formatacao de data e moeda

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -7,6 +7,8 @@ import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import Months from '../../utils/months';
 import Years from '../../utils/years';
+import formatCurrency from '../../utils/formatCurrency';
+import formatDate from '../../utils/formatDate';
 
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
@@ -53,9 +55,9 @@ const List: React.FC<IRouteParams> = ({ match }) => {
       return {
         id: String(Math.random() * data.length),
         description: item.description,
-        amountFormatted: item.amount,
+        amountFormatted: formatCurrency(Number(item.amount)),
         frequency: item.frequency,
-        dateFormatted: item.date,
+        dateFormatted: formatDate(item.date),
         tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
       }
     })
